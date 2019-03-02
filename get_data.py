1ed13e95db14ddcf2900a6e60462e9203375af0a
Ramansh Sharma <sharmar@bxscience.edu>
2019-03-02 23:17:50 +0530

    params needed in functions for specifications

--- get_data.py
+++ get_data.py
@@ -33,58 +33,58 @@ class Data:
 
     def get_from(self, media):  # receive a link to sign in into
         # TODO : a social media name is given in string, then is signed into, and collected data from
-        return self.connector_to_website[media]()
+        return self.connector_to_website[media](param=media)
 
 
-def get_school():
+def get_school(param=None):
     # TODO : get latest home page pics and articles from Bronx Science's website and the Science Survey website
     pass
 
 
-def get_twitter():
+def get_twitter(param=None):
     # TODO : get data such as number of followers and following, a few of their names, also some of the tweets from my
     #  favorite twitter accounts
     pass
 
 
-def get_linkedin():
+def get_linkedin(param=None):
     # TODO : sign in and get data from the linkedin page; connections and possibly some top stories in the feed
     pass
 
 
-def get_instagram():
+def get_instagram(param=None):
     # TODO : get my followers list of names, following list of names, and pics from some accounts
     pass
 
 
-def get_github():
+def get_github(param=None):
     # TODO : get list of followers and following names, plus some news from github home page
     pass
 
 
-def get_kaggle():
+def get_kaggle(param=None):
     # TODO : get some interesting articles from kaggle homepage (first five)
     pass
 
 
-def get_mail():
+def get_mail(param=None):
     # TODO : the email themselves will be signed into in another module, here I just want to see if I have any new
     #  unread emails, if so, what are their subjects
     pass
 
 
-def get_xkcd():
+def get_xkcd(param=None):
     # TODO : get me the last 10 articles from the xkcd blog, AND 10 random pics from the xkcd comic. Well, the first
     #  pic from the comic has to be after random is pressed, I already saw the Carbonated Beverage one
     # For default right now, I will only access the main article page of xkcd. Learning requests here
-    response = requests.get()
+    response = requests.get(commands[param])
     try:
         response.raise_for_status()
 
     except Exception as exc:
         print('There was a problem: %s' % exc)
 
-    file = open('RomeoAndJuliet.txt', 'wb')
+    file = open('file.txt', 'wb')
     for chunk in response.iter_content(100000):
         file.write(chunk)
     file.close()
