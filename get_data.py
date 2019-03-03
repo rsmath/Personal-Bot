7cee1b0e10e55ae439b0ea25ccacde1d4332bfa1
Ramansh Sharma <sharmar@bxscience.edu>
2019-03-03 22:34:38 +0530

    removing error messages

--- get_data.py
+++ get_data.py
@@ -36,43 +36,35 @@ class Data:
         # TODO : a social media name is given in string, then is signed into, and collected data from
         return self.connector_to_website[media](param=media)
 
-
     def get_school(self, param=None):
         # TODO : get latest home page pics and articles from Bronx Science's website and the Science Survey website
         pass
-
 
     def get_twitter(self, param=None):
         # TODO : get data such as number of followers and following, a few of their names, also some of the tweets from my
         #  favorite twitter accounts
         pass
 
-
     def get_linkedin(self, param=None):
         # TODO : sign in and get data from the linkedin page; connections and possibly some top stories in the feed
         pass
-
 
     def get_instagram(self, param=None):
         # TODO : get my followers list of names, following list of names, and pics from some accounts
         pass
 
-
     def get_github(self, param=None):
         # TODO : get list of followers and following names, plus some news from github home page
         pass
-
 
     def get_kaggle(self, param=None):
         # TODO : get some interesting articles from kaggle homepage (first five)
         pass
 
-
     def get_mail(self, param=None):
         # TODO : the email themselves will be signed into in another module, here I just want to see if I have any new
         #  unread emails, if so, what are their subjects
         pass
-
 
     def get_xkcd(self, param=None):
         # TODO : get me the last 10 articles from the xkcd blog, AND 10 random pics from the xkcd comic. Well, the first
@@ -86,7 +78,7 @@ class Data:
             print('There was a problem: %s' % exc)
 
         file = open('file.txt', 'wb')
-        self.bsoup = bs.BeautifulSoup(response.text)
+        self.bsoup = bs.BeautifulSoup(response.text, features="html.parser")
 
 
 if __name__ == '__main__':
