2c4389ba35f1cddd03c7624894a51fcc2e7fd8b9
Ramansh Sharma <sharmar@bxscience.edu>
2019-03-03 22:16:49 +0530

    working on getting data

--- get_data.py
+++ get_data.py
@@ -16,78 +16,77 @@ class Data:
         # TODO : Initiate variables
         self.media = ''
         self.connector_to_website = {
-            "twitter": get_twitter,
-            "instagram": get_instagram,
-            "github": get_github,
-            "linkedin": get_linkedin,
-            "kaggle": get_kaggle,
-            "mail": get_mail,
-            "mail0": get_mail,
-            "mail1": get_mail,
-            "mail2": get_mail,
-            "xkcd": get_xkcd,
-            "blog": get_xkcd,
-            "school": get_school,  # different param from below one
-            "sciencesurvey": get_school  # different param from above one
+            "twitter": self.get_twitter,
+            "instagram": self.get_instagram,
+            "github": self.get_github,
+            "linkedin": self.get_linkedin,
+            "kaggle": self.get_kaggle,
+            "mail": self.get_mail,
+            "mail0": self.get_mail,
+            "mail1": self.get_mail,
+            "mail2": self.get_mail,
+            "xkcd": self.get_xkcd,
+            "blog": self.get_xkcd,
+            "school": self.get_school,  # different param from below one
+            "sciencesurvey": self.get_school  # different param from above one
         }
+        self.bsoup = None
 
     def get_from(self, media):  # receive a link to sign in into
         # TODO : a social media name is given in string, then is signed into, and collected data from
         return self.connector_to_website[media](param=media)
 
 
-def get_school(param=None):
-    # TODO : get latest home page pics and articles from Bronx Science's website and the Science Survey website
+    def get_school(self, param=None):
+        # TODO : get latest home page pics and articles from Bronx Science's website and the Science Survey website
+        pass
+
+
+    def get_twitter(self, param=None):
+        # TODO : get data such as number of followers and following, a few of their names, also some of the tweets from my
+        #  favorite twitter accounts
     pass
 
 
-def get_twitter(param=None):
-    # TODO : get data such as number of followers and following, a few of their names, also some of the tweets from my
-    #  favorite twitter accounts
-    pass
+    def get_linkedin(self, param=None):
+        # TODO : sign in and get data from the linkedin page; connections and possibly some top stories in the feed
+        pass
 
 
-def get_linkedin(param=None):
-    # TODO : sign in and get data from the linkedin page; connections and possibly some top stories in the feed
-    pass
+    def get_instagram(self, param=None):
+        # TODO : get my followers list of names, following list of names, and pics from some accounts
+        pass
 
 
-def get_instagram(param=None):
-    # TODO : get my followers list of names, following list of names, and pics from some accounts
-    pass
+    def get_github(self, param=None):
+        # TODO : get list of followers and following names, plus some news from github home page
+        pass
 
 
-def get_github(param=None):
-    # TODO : get list of followers and following names, plus some news from github home page
-    pass
+    def get_kaggle(self, param=None):
+        # TODO : get some interesting articles from kaggle homepage (first five)
+        pass
 
 
-def get_kaggle(param=None):
-    # TODO : get some interesting articles from kaggle homepage (first five)
-    pass
+    def get_mail(self, param=None):
+        # TODO : the email themselves will be signed into in another module, here I just want to see if I have any new
+        #  unread emails, if so, what are their subjects
+        pass
 
 
-def get_mail(param=None):
-    # TODO : the email themselves will be signed into in another module, here I just want to see if I have any new
-    #  unread emails, if so, what are their subjects
-    pass
+    def get_xkcd(self, param=None):
+        # TODO : get me the last 10 articles from the xkcd blog, AND 10 random pics from the xkcd comic. Well, the first
+        #  pic from the comic has to be after random is pressed, I already saw the Carbonated Beverage one
+        # For default right now, I will only access the main article page of xkcd. Learning requests here
+        response = requests.get(commands[param])
+        try:
+            response.raise_for_status()
 
+        except Exception as exc:
+            print('There was a problem: %s' % exc)
 
-def get_xkcd(param=None):
-    # TODO : get me the last 10 articles from the xkcd blog, AND 10 random pics from the xkcd comic. Well, the first
-    #  pic from the comic has to be after random is pressed, I already saw the Carbonated Beverage one
-    # For default right now, I will only access the main article page of xkcd. Learning requests here
-    response = requests.get(commands[param])
-    try:
-        response.raise_for_status()
-
-    except Exception as exc:
-        print('There was a problem: %s' % exc)
-
-    file = open('file.txt', 'wb')
-    for chunk in response.iter_content(100000):
-        file.write(chunk)
-    file.close()
+        file = open('file.txt', 'wb')
+        self.bsoup =
 
 
 if __name__ == '__main__':
