406b2aa6ce5e2147f89486e9d448dc7dd8a4cfc9
Ramansh Sharma <sharmar@bxscience.edu>
2019-03-03 23:10:47 +0530

    why is this not working?

--- get_data.py
+++ get_data.py
@@ -78,7 +78,7 @@ class Data:
         file = open('file.txt', 'wb')
         self.bsoup = bs.BeautifulSoup(response.text, features="html.parser")
         article = self.bsoup.select('article.entry')
-        article_url = 'https:' + article[0].get('src')
+        article_url = 'https:' + article[0].get('href')
         res = requests.get(article_url)
         if check_res(response) is not None:
             print(check_res(response))
@@ -86,6 +86,9 @@ class Data:
 
         for chunk in res.iter_content(100000):
             file.write(chunk)
+
+        file.close()
+
 
 def check_res(res):
     try:
